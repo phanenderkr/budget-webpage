0104a0c9f6e2cb1dbca95ddaf1f374085056035d
Phanender Reddy Kasireddy <phanender100@gmail.com>
2017-12-05 19:38:24 +0530

    Add items to data controller

--- app.js
+++ app.js
@@ -1,6 +1,60 @@
 //Budget data controller
 var budgetController = (function () {
 
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description =description ;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description =description ;
+        this.value = value;
+    };
+
+    var data ={
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        totals:{
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //create new ID
+            if(data.allItems[type].length>0){
+                ID = ( data.allItems[type] )[data.allItems[type].length-1].id + 1;
+            }else{
+                ID = 0;
+            }
+
+
+            //create new item based on inc or exp type
+            if(type.toLowerCase()==="exp"){
+                newItem = new Expense(ID, des, val);
+            }else{
+                newItem = new Income(ID, des, val);
+            }
+
+            //pushing to our data structure
+            data.allItems[type].push(newItem);
+
+            //returning the item
+            return newItem;
+        },
+        testing: function () {
+            console.log(data);
+        }
+
+
+    }
 
 })();
 
@@ -15,6 +69,7 @@ var UIController = (function () {
         tickBtn: ".add__btn"
     };
 
+    // returning object
     return{
         getInput: function () {
             return{
@@ -33,14 +88,28 @@ var UIController = (function () {
 //Main Controller
 var controller = (function (budgetCntr, UICntrl) {
 
-    var DOM = UICntrl.getDOMStrings();
+    var setupEventListeners = function () {
+
+        var DOM = UICntrl.getDOMStrings();
+
+        document.querySelector(DOM.tickBtn).addEventListener('click', controlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if(event.keyCode ===13 || event.which===13){
+                controlAddItem();
+            }
+
+        });
+    };
 
     var controlAddItem = function () {
 
         //1. Get input data
         var input = UICntrl.getInput();
         console.log(input);
+
         //2. Add to budget data controller
+        var newItem = budgetCntr.addItem(input.type, input.description, input.value);
 
         //3. Add item to the UI
 
@@ -50,13 +119,12 @@ var controller = (function (budgetCntr, UICntrl) {
 
     };
 
-
-    document.querySelector(DOM.tickBtn).addEventListener('click', controlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-        if(event.keyCode ===13 || event.which===13){
-            controlAddItem();
+    return{
+        init: function () {
+            setupEventListeners();
         }
+    }
 
-    })
 })(budgetController, UIController);
+
+controller.init();
